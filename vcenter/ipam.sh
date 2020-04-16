27d21176178104368c5ab23d76215f78cc137ea4
Simyung Yang <ysm821205@gmail.com>
2020-04-17 02:08:41 +0900

    first commit

---
+++ vcenter/ipam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "DnsServerList=8.8.8.8,8.8.4.4"
+echo "nicCount=1"  # This is a mandatory parameter and is always set to 1
+echo "nicIP_0=10.72.86.173"
+echo "nicDnsServerList_0=8.8.8.8,8.8.4.4"
+echo "nicGateway_0=10.72.86.1"
+echo "nicNetmask_0=255.255.255.0"
+echo "domainName=localhost"
+echo "hwClockUTC=true"
+echo "timeZone=Canada/Eastern"
+echo "osHostname=testhost1"
