4432633f23350aaed839e3268fe99dd66732e293
Simyung Yang <ysm821205@gmail.com>
2020-04-17 17:31:55 +0900

    service script comment out

--- vm-init.sh
+++ vm-init.sh
@@ -12,12 +12,16 @@
 CLIQR_HOME=/usr/local/osmosix
 . $CLIQR_HOME/etc/.osmosix.sh
 . $CLIQR_HOME/etc/userenv
-. $CLIQR_HOME/service/utils/cfgutil.sh
-. $CLIQR_HOME/service/utils/install_util.sh
-. $CLIQR_HOME/service/utils/os_info_util.sh
+#. $CLIQR_HOME/service/utils/cfgutil.sh
+#. $CLIQR_HOME/service/utils/install_util.sh
+#. $CLIQR_HOME/service/utils/os_info_util.sh
 
 # Main
 sudo agentSendLogMessage "### STARTING VM POST-INIT ###"
+
+# Hostname
+hostnamectl set-hostname $HOSTNAME
+
 
 ## Create new user
 sudo agentSendLogMessage "Creating new user..."
