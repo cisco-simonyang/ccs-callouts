09902ed649f716bda3d235512fec8e57f17d413a
Simyung Yang <ysm821205@gmail.com>
2020-04-20 13:12:05 +0900

    .

--- vm-init.sh
+++ vm-init.sh
@@ -13,6 +13,7 @@ CLIQR_HOME=/usr/local/osmosix
 . $CLIQR_HOME/etc/.osmosix.sh
 . $CLIQR_HOME/etc/userenv
 . $CLIQR_HOME/service/utils/cfgutil.sh
+. $CLIQR_HOME/utils/agent_util.sh
 . $CLIQR_HOME/service/utils/install_util.sh
 . $CLIQR_HOME/service/utils/os_info_util.sh
 
