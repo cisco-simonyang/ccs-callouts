450edfc6e1ab733d6a32b9b11c33b0a14a869aff
Simyung Yang <ysm821205@gmail.com>
2020-04-20 13:36:57 +0900

    test script added

---
+++ vm-init-test.sh
@@ -0,0 +1,9 @@
+CLIQR_HOME=/usr/local/osmosix
+. $CLIQR_HOME/etc/.osmosix.sh
+. $CLIQR_HOME/etc/userenv
+. $CLIQR_HOME/utils/agent_util.sh
+. $CLIQR_HOME/service/utils/cfgutil.sh
+. $CLIQR_HOME/service/utils/install_util.sh
+. $CLIQR_HOME/service/utils/os_info_util.sh
+
+touch /home/cliqruser/abc
